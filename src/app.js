40db3d231995e565719f5294e9fd2a59563384a1
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-01 22:14:50 +0200

    feat: Empty /post router and formatting of app.js

--- src/app.js
+++ src/app.js
@@ -2,17 +2,26 @@ import express from "express";
 import process from "process";
 import session from "express-session";
 import passport from "passport";
+import postRouter from "./routes/postRouter.js";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(session({ secret: process.env.SECRET_SESSION, resave: false, saveUninitialized: false }));
+app.use(
+    session({
+        secret: process.env.SECRET_SESSION,
+        resave: false,
+        saveUninitialized: false,
+    }),
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("/");
 });
+
+app.use("/post", postRouter);
 
 app.listen(PORT);
