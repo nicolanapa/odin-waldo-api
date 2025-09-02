924d4338e0ce56e9b3241c9c14d5bd32af94ded9
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-02 22:28:56 +0200

    feat: GET /:id and GET /:id/leaderboard
    These two paths would return a custom result from a normal query to make it more readable from a Client
    
    Added empty paths TBD soon

--- src/routes/photoRouter.js
+++ src/routes/photoRouter.js
@@ -7,4 +7,76 @@ photoRouter.get("/", async (req, res) => {
     return res.json(await prisma.photo.findMany({ select: { id: true } }));
 });
 
+photoRouter.post("/", async (req, res) => {});
+
+photoRouter.get("/:id", async (req, res) => {
+    let photoElement = await prisma.photo.findUnique({
+        where: {
+            id: parseInt(req.params.id),
+        },
+        select: {
+            id: true,
+            link: true,
+            characters: {
+                select: {
+                    name: true,
+                },
+            },
+        },
+    });
+
+    photoElement = {
+        ...photoElement,
+        characters: photoElement.characters.map(
+            (character) => character["name"],
+        ),
+    };
+
+    return res.json(photoElement);
+});
+
+photoRouter.get("/:id/leaderboard", async (req, res) => {
+    let leaderboard = await prisma.score.findMany({
+        where: {
+            photoId: parseInt(req.params.id),
+        },
+        select: {
+            startTime: true,
+            endTime: true,
+            user: {
+                select: {
+                    name: true,
+                },
+            },
+        },
+    });
+
+    for (let i = 0; i < leaderboard.length; i++) {
+        leaderboard[i].score =
+            (leaderboard[i].endTime - leaderboard[i].startTime) / 1000;
+
+        leaderboard[i].user = leaderboard[i].user.name;
+    }
+
+    console.log(leaderboard);
+
+    return res.json(leaderboard);
+    
+    /*return res.json(
+        await prisma.$queryRaw`
+            SELECT EXTRACT(EPOCH FROM ("endTime" - "startTime")) AS "score", "userId" FROM "Score"
+            WHERE "photoId" = ${parseInt(req.params.id)}
+            ORDER BY "score" ASC;
+        `,
+    );*/
+});
+
+photoRouter.post("/:id/checkPosition", async (req, res) => {});
+
+photoRouter.post("/:id/start", async (req, res) => {});
+
+photoRouter.post("/:id/end", async (req, res) => {});
+
+photoRouter.post("/:id/confirm", async (req, res) => {});
+
 export default photoRouter;
