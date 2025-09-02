4568d3129e9429ef0a4f6f024d223672300db764
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-02 20:44:57 +0200

    feat: Select in GET / to return only IDs

--- src/routes/photoRouter.js
+++ src/routes/photoRouter.js
@@ -4,7 +4,7 @@ import prisma from "../db/prisma.js";
 const photoRouter = new Router();
 
 photoRouter.get("/", async (req, res) => {
-    return res.json(await prisma.photo.findMany());
+    return res.json(await prisma.photo.findMany({ select: { id: true } }));
 });
 
 export default photoRouter;
