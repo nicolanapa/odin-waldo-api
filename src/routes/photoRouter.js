3718f4d07a58370ec9d6e872462d451da878d1ec
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-04 23:45:50 +0200

    feat: POST /:id/end which ads a endTime key used in POST /:id/confirm

--- src/routes/photoRouter.js
+++ src/routes/photoRouter.js
@@ -19,6 +19,15 @@ const positionValidation = [
         .withMessage("Vertical value must be a Number"),
 ];
 
+const nameValidation = [
+    body("name")
+        .trim()
+        .escape()
+        .default("anon")
+        .isLength({ min: 2, max: 32 })
+        .withMessage("name must be between 2 and 32 characters length"),
+];
+
 const photoRouter = new Router();
 
 photoRouter.get("/", async (req, res) => {
@@ -136,8 +145,55 @@ photoRouter.post("/:id/start", async (req, res) => {
 
 photoRouter.post("/:id/end", async (req, res) => {
     console.log(req.header("Authorization"));
+
+    if (!req.header("Authorization").startsWith("Bearer ")) {
+        return res
+            .status(400)
+            .json({ error: "JWT may be wrong or has expired" });
+    }
+
+    let token = req.header("Authorization").substring(7);
+    let decodedJwt = null;
+
+    try {
+        decodedJwt = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ error: "JWT may be wrong or has expired" });
+    }
+
+    console.log(decodedJwt);
+
+    if (
+        decodedJwt.ip !== req.ip ||
+        decodedJwt.postId !== parseInt(req.params.id)
+    ) {
+        return res
+            .status(400)
+            .json({ error: "JWT may be wrong or has expired" });
+    }
+
+    let finalJwt = {
+        startTime: decodedJwt.startTime,
+        endTime: new Date(),
+        postId: decodedJwt.postId,
+        ip: decodedJwt.ip,
+    };
+
+    finalJwt = jwt.sign(finalJwt, process.env.JWT_PRIVATE_KEY, {
+        expiresIn: "5m",
+    });
+
+    res.status(200).json(finalJwt);
 });
 
-photoRouter.post("/:id/confirm", async (req, res) => {});
+photoRouter.post("/:id/confirm", nameValidation, async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+});
 
 export default photoRouter;
